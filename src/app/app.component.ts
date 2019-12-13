b0f559c0c9c9dcd5477275de04fa217a71a74d2d
Mauribeza <uribez.manuela@gmail.com>
2019-12-13 17:43:39 -0500

    Documentacion del código

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,16 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  arr = [1,2,3];
+  arr = [1];
+  index = 2;
   ngOnInit(): void {
-    from(this.arr).pipe(map(i => i * 8)).subscribe(_ => console.log(_));
+    from(this.arr).subscribe(_ => console.log(_));
 
-    setTimeout(() => {
-      this.arr.push(8)
-    },  5000);
+      setInterval(() => {
+        if(this.index <= 5){
+          this.arr.push(this.index++)
+        }
+      },  2000);
   }
 
   mostrarArray(){
