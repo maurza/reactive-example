3f47313e048e6664b61e7bed838daedce1fe5fa3
paolaPosada <paola.posada1@udea.edu.co>
2019-12-14 03:38:37 -0500

    Documentacion again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,11 @@ import { map } from 'rxjs/operators';
 export class AppComponent implements OnInit {
   arr = [1];
   index = 2;
+// Es el metodo que se ejecuta cuando se inicializa el componente
   ngOnInit(): void {
+// Crea el observable y se suscribe a el
     from(this.arr).subscribe(_ => console.log(_));
-
+// Cada dos segundos se ejecuta la funcion en este caso se ejecuta la suscripción
       setInterval(() => {
         if(this.index <= 5){
           this.arr.push(this.index++)
